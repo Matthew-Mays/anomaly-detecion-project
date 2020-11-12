52176f4d52fc0d7db235247e82c1a8647fe164d5
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-11-12 08:51:22 -0600

    fixed a spelling mistake that caused issues

--- prepare.py
+++ prepare.py
@@ -1,15 +1,26 @@
-import pandas as import pd
+import pandas as pd
 from acquire import acquire_codeup_data
 
 def prep_codeup_data():
-    df = acquire_codeup_data()
-    df.timestamp = df.timestamp.str.replace(r'(\[|\])', '', regex=True)
-    df.timestamp = pd.to_datetime(df.timestamp.str.replace(':', ' ', 1))
-    df = df.set_index('timestamp')
-    for col in ['request_method', 'request_agent', 'destination']:
-        df[col] = df[col].str.replace('"', '')
-    df['request_method'] = df.request_method.str.replace(r'\?page=[0-9]+', '', regex=True)
-    df[['request_call','api_version','endpoints','http']] = \
-    df.request_method.str.extract(r'(?P<request_call>^[A-Z]+)\s(?P<api_version>\/api\/v[0-9])(?P<endpoints>.+)(?P<http_version>HTTP\/[0-9]\.[0-9])', expand = True)
-    df['size_mb'] = [n/1024/1024 for n in df['size']]
+    df, cohorts = acquire_codeup_data()
+    df.columns = ['date', 'time', 'page_viewed', 'user_id', 'cohort_id', 'ip']
+    df['datetime'] = df['date'] + ' ' + df['time']
+    df['datetime'] = pd.to_datetime(df.datetime)
+    df['year'] = df.datetime.dt.year
+    df['month'] = df.datetime.dt.month
+    df['day'] = df.datetime.dt.day
+    df['hour'] = df.datetime.dt.hour
+    df['weekday'] = df.datetime.dt.day_name()
+    df = df.astype(object)
+    df.drop(columns=['date', 'time'], inplace=True)
+    combine = pd.merge(left = df, right = cohorts, how = 'left', left_on = 'cohort_id', right_on = 'cohort_id')
+    combine = combine.set_index('datetime')
+    return combine
+
+def handle_nulls(df):
+    df['cohort_id'] = df['cohort_id'].fillna(0)
+    df['name'] = df['name'].fillna('unknown')
+    df['start_date'] = df['start_date'].fillna('01-01-1900')
+    df['end_date'] = df['end_date'].fillna('01-01-1900')
+    df['program_id'] = df['program_id'].fillna(0)
     return df
